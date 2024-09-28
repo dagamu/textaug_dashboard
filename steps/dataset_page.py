cb7b8120d2477ee7223ddeb3afb141ee3f19a45f
dagamu <gajardomunoz@gmail.com>
2024-09-28 18:03:47 -0300

    [Dataset Loading] OOP Refactoring

--- steps/dataset_page.py
+++ steps/dataset_page.py
@@ -2,23 +2,66 @@ import pandas as pd
 import importlib
 import os
 
-def UploadTab(st):
-    st.subheader("Load a CSV file")
-    return st.file_uploader( "Choose a CSV file", accept_multiple_files=False ) 
-
-def DataFolderTab(st):
-    st.subheader("Select from data folder")
-    file_options = [ file for file in os.listdir("data") if file.endswith(".csv") ]
-    return st.selectbox("CSV files in /data folder", file_options, index=None ) 
-
-def HuggingFaceTab(st):
-    st.subheader("Load a dataset from Hugging Face Datasets Module") 
-    st.info("trust_remote_code is activated", icon="ℹ")
-    df_split = st.selectbox("Dataset Split", ["train", "validation", "test", "train+test", "train+test+validation"])
-    df_name = st.text_input("Dataset Name", placeholder="user/dataset")
-    st.text("Common Examples: qanastek/HoC")
-    return df_split, df_name
+class FileTab:
+    def __init__(self):
+        self.name = "FileUpload"
+        
+    def get_data(self):
+        filename = self.filepath.name
+        return pd.read_csv(self.filepath), filename
     
+    def render(self, st):
+        st.subheader("Load a CSV file")
+        self.filepath = st.file_uploader( "Choose a CSV file", accept_multiple_files=False )
+
+class DataFolderTab:
+    def __init__(self):
+        self.name = "DataFolder"
+        
+    def get_data(self):
+        return pd.read_csv(f"data/{self.filename}"), self.filename
+    
+    def render(self, st):
+        st.subheader("Select from data folder")
+        
+        file_options = [ file for file in os.listdir("data") if file.endswith(".csv") ]
+        self.filename = st.selectbox("CSV files in /data folder", file_options, index=None )
+
+class HuggingFaceTab:
+    def __init__(self):
+        self.name = "Hugging Face"
+        
+    def get_data(self):
+        load_dataset = importlib.import_module("datasets").load_dataset
+        df_name = self.df_key.split('/')[1]
+        df = load_dataset(self.df_key, split=self.df_split, trust_remote_code=True).to_pandas()
+        
+        return df, df_name
+    
+    def render(self, st):
+        st.subheader("Load a dataset from Hugging Face Datasets Module") 
+        
+        st.info("trust_remote_code is activated", icon="ℹ")
+        
+        self.df_split = st.selectbox("Dataset Split", ["train", "validation", "test", "train+test", "train+test+validation"])
+        self.df_key = st.text_input("Dataset Name", placeholder="user/dataset")
+        st.text("Common Examples: qanastek/HoC")
+
+
+def UploadBtn(st, method):
+    btn_text = "Upload another dataset" if "df" in st.session_state else "Upload"
+    if st.button(btn_text, type="primary", key=method.name):
+        st.toast('Loading Dataset')
+        df, df_name = method.get_data()
+        st.toast('Done!')
+        
+        st.session_state['df_name'] = df_name
+        st.session_state['df'] = df
+         
+        ## Clear prev properties
+        for prop in ['labels_column', 'all_labels']:
+            if prop in st.session_state:
+                del st.session_state[prop]
 
 def DatasetPage(st):
     
@@ -27,49 +70,15 @@ def DatasetPage(st):
     if "df" in st.session_state:
         st.markdown(f"**Selected Dataset: {st.session_state['df_name']}**" )
     
-    upload_tab, data_folder_tab, hf_tab = st.tabs([ "Upload file", "Data Folder", "Hugging Face" ])
+    load_methods =  {
+        "Upload file":  FileTab,
+        "Data Folder":  DataFolderTab,
+        "Hugging Face": HuggingFaceTab,
+    } 
     
-    with upload_tab:
-        uploaded_file = UploadTab(st)
-        
-    with data_folder_tab:
-        selected_data = DataFolderTab(st)
+    for method, tab in zip( load_methods.values(), st.tabs(load_methods.keys()) ):
+        with tab:
+            load_method = method()
+            load_method.render(st)
+            UploadBtn( st, load_method )
     
-    with hf_tab:
-        hf_split, hf_dataset = HuggingFaceTab(st)
-    
-    
-    st.text("")
-    
-    btn_text = "Upload another dataset" if "df" in st.session_state else "Upload"
-    if st.button(btn_text, type="primary"):
-        
-        
-        if hf_dataset:
-            load_dataset = importlib.import_module("datasets").load_dataset
-            df_name = hf_dataset.split('/')[1]
-            st.toast('Loading Dataset')
-            df = load_dataset(hf_dataset, split=hf_split, trust_remote_code=True).to_pandas()
-            st.toast("Done!")
-        
-        else:
-            if uploaded_file:
-                csv_path = uploaded_file
-                df_name = uploaded_file.name
-                
-            else:
-                csv_path = f"data/{selected_data}"
-                df_name = selected_data
-                
-            st.toast('Loading Dataset')
-            df = pd.read_csv(csv_path)
-            st.toast("Done!")
-        
-        st.session_state['df_name'] = df_name
-        st.session_state['df'] = df
-        
-        
-        ## Clear prev properties
-        for prop in ['labels_column', 'all_labels']:
-            if prop in st.session_state:
-                del st.session_state[prop]
